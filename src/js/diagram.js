eb93cf4b660ff94f7c745f10361811a2941db4dc
Emma Larsson <emma940112@gmail.com>
2024-03-08 09:31:49 +0100

    Hämtat data från en extern källa och lagt in denna i diagrammen

--- src/js/diagram.js
+++ src/js/diagram.js
@@ -1,45 +1,90 @@
-//Stapeldiagram
-let xValues = ["Kurs1", "Kurs2", "Kurs3", "Kurs4", "Kurs5", "Kurs6"];
-let yValues = [10, 10, 10, 10, 10, 10];
-let barColors = ["red", "green", "blue", "orange", "brown"];
+"use strict";
 
-new Chart("barChart", {
-    type: "bar",
-    data: {
-        labels: xValues,
-        datasets: [{
-            backgroundColor: barColors,
-            data: yValues
-        }]
-    },
-    options: {
-        legend: {display: false},
-        title: {
-            display: true,
-            text: "De mest sökta kurserna på Mittuniversitetet"
-        }
+const url = ("https://studenter.miun.se/~mallar/dt211g/");
+window.onload = init();
+
+async function init() {
+    try {
+        let coursesAndPrograms = [];
+        //Fetch-anrop
+        const response = await fetch(url);
+        coursesAndPrograms = await response.json();
+
+        //Filtrera ut alla kurser och program
+        const onlyCourses = coursesAndPrograms.filter(course => course.type === "Kurs");
+        const onlyPrograms = coursesAndPrograms.filter(program => program.type === "Program");
+
+        //Sortera alla kurser och program efter antal sökande
+        onlyCourses.sort((a, b) => (b.applicantsTotal - a.applicantsTotal));
+        onlyPrograms.sort((a, b) => (b.applicantsTotal - a.applicantsTotal));
+
+        //Spara de sex mest sökta kurserna och de fem mest sökta programmen
+        const topCourses = onlyCourses.slice(0, 6);
+        const topPrograms = onlyPrograms.slice(0, 5);
+
+        //Spara de namnen på de sex mest sökta kurserna och de fem mest sökta programmen i en array
+        const courseName = topCourses.map(course => {
+            return course.name;
+        });
+        const programName = topPrograms.map(program => {
+            return program.name;
+        });
+
+        //Spara antalet sökande till kurserna och programmen i en array
+        const courseApplicants = topCourses.map(course => {
+            return course.applicantsTotal;
+        });
+        const programApplicants = topPrograms.map(program => {
+            return program.applicantsTotal;
+        });
+
+        //Stapeldiagram
+        let xValues = courseName;
+        let yValues = courseApplicants;
+        let barColors = ["red", "green", "blue", "orange", "brown"];
+
+        new Chart("barChart", {
+            type: "bar",
+            data: {
+                labels: xValues,
+                datasets: [{
+                    backgroundColor: barColors,
+                    data: yValues
+                }]
+            },
+            options: {
+                legend: { display: false },
+                title: {
+                    display: true,
+                    text: "De mest sökta kurserna på Mittuniversitetet"
+                }
+            }
+        });
+
+        //Cirkeldiagram
+        let xValues2 = programName;
+        let yValues2 = programApplicants;
+        let barColors2 = ["red", "green", "blue", "orange", "brown"];
+
+        new Chart("pieChart", {
+            type: "pie",
+            data: {
+                labels: xValues2,
+                datasets: [{
+                    backgroundColor: barColors2,
+                    data: yValues2
+                }]
+            },
+            options: {
+                legend: { display: false },
+                title: {
+                    display: true,
+                    text: "De mest sökta programmen på Mittuniversitetet"
+                }
+            }
+        });
+
+    } catch {
+        document.getElementById("error").innerHTML = "<p>Något gick fel, prova igen senare.</p>"
     }
-});
-
-//Cirkeldiagram
-let xValues2 = ["Program1", "Program2", "Program3", "Program4", "Program5"];
-let yValues2 = [10, 10, 10, 10, 10];
-let barColors2 = ["red", "green", "blue", "orange", "brown"];
-
-new Chart("pieChart", {
-    type: "pie",
-    data: {
-        labels: xValues2,
-        datasets: [{
-            backgroundColor: barColors2,
-            data: yValues2
-        }]
-    },
-    options: {
-        legend: {display: false},
-        title: {
-            display: true,
-            text: "De mest sökta programmen på Mittuniversitetet"
-        }
-    }
-});
+}
