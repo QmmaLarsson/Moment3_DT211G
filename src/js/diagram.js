456c34ed491c48df06ee1368e354f074dc13b6e8
Emma Larsson <emma940112@gmail.com>
2024-03-09 09:19:13 +0100

    Installerade chart.js som ett npm paket, snyggat till diagrammet, har bland annat gjort så att texten under stapeldiagrammet ligger på horisontella rader

--- src/js/diagram.js
+++ src/js/diagram.js
@@ -1,5 +1,6 @@
 "use strict";
 
+import Chart from 'chart.js/auto';
 const url = ("https://studenter.miun.se/~mallar/dt211g/");
 window.onload = init();
 
@@ -23,8 +24,8 @@ async function init() {
         const topPrograms = onlyPrograms.slice(0, 5);
 
         //Spara de namnen på de sex mest sökta kurserna och de fem mest sökta programmen i en array
-        const courseName = topCourses.map(course => {
-            return course.name;
+        let courseName = topCourses.map(course => {
+            return course.name.split(' ');
         });
         const programName = topPrograms.map(program => {
             return program.name;
@@ -41,46 +42,62 @@ async function init() {
         //Stapeldiagram
         let xValues = courseName;
         let yValues = courseApplicants;
-        let barColors = ["red", "green", "blue", "orange", "brown"];
+        let barColors = ["red", "purple", "blue", "pink", "yellow", "green"];
 
         new Chart("barChart", {
             type: "bar",
             data: {
                 labels: xValues,
                 datasets: [{
+                    label: "Antal sökande",
                     backgroundColor: barColors,
                     data: yValues
                 }]
             },
             options: {
-                legend: { display: false },
-                title: {
-                    display: true,
-                    text: "De mest sökta kurserna på Mittuniversitetet"
-                }
-            }
+                responsive: true,
+                plugins: {
+                    legend: {
+                        display: false
+                    },
+                    tooltip: {
+                        callbacks: {
+                            title: (context) => {
+                                return (context[0].label.replaceAll(",", " "));
+                            },
+                        },
+                    },
+                },
+            },
         });
+
 
         //Cirkeldiagram
         let xValues2 = programName;
         let yValues2 = programApplicants;
-        let barColors2 = ["red", "green", "blue", "orange", "brown"];
+        let barColors2 = ["red", "purple", "blue", "pink", "yellow"];
 
         new Chart("pieChart", {
             type: "pie",
             data: {
                 labels: xValues2,
-                datasets: [{
-                    backgroundColor: barColors2,
-                    data: yValues2
-                }]
+                datasets: [
+                    {
+                        label: "Antal sökande",
+                        data: yValues2,
+                        backgroundColor: barColors2
+                    }]
             },
             options: {
-                legend: { display: false },
-                title: {
-                    display: true,
-                    text: "De mest sökta programmen på Mittuniversitetet"
+                responsive: true,
+                maintainAspectRatio: true,
+                plugins: {
+                    legend: {
+                        display: true,
+                        position: "left",
+                    }
                 }
+
             }
         });
 
